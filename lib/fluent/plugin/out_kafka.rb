c6510ec677e6745b74efa0169a8c1e0df679f5d8
liyong <lyon.liquid@gmail.com>
2013-07-29 15:40:10 +0800

    modify out_kafka.rb

--- lib/fluent/plugin/out_kafka.rb
+++ lib/fluent/plugin/out_kafka.rb
@@ -4,12 +4,38 @@ class Fluent::KafkaOutput < Fluent::BufferedOutput
   def initialize
     super
     require 'kafka'
+
+#extra info feature,added by liyong
+    require 'socket'
+    @host_local = Socket.gethostname
+    @ip_local = Socket::getaddrinfo(@host_local, Socket::SOCK_STREAM)[0][3]
+    @idc = @host_local.split("-")[0]
+    if @need_platform_info
+      require 'nodes'
+      im_nodes = Nodes.new()
+      result = im_nodes.search_tags(@host_local.split(".baidu.com")[0])
+      if result["status"] != 0
+        @platform = ""
+      else
+        mid_array = result["data"]["tags"]
+        platform_list = mid_array.select{|x| x[0] == "PLATFORM"}
+        platform_list.each do |y|
+          @platform = /[A-Za-z]+[0-9]+/.match(y[1]).to_s
+          break if @platform != ''
+        end
+      end
+    end
+####################################
+
   end
 
   config_param :host, :string, :default => 'localhost'
   config_param :port, :integer, :default => 9092
+  config_param :zkhost, :string, :default => nil
+  config_param :zkport, :integer, :default => nil
   config_param :default_topic, :string, :default => nil
   config_param :default_partition, :integer, :default => 0
+  config_param :need_platform_info, :bool, :default => false
 
   def configure(conf)
     super
@@ -33,6 +59,12 @@ class Fluent::KafkaOutput < Fluent::BufferedOutput
     chunk.msgpack_each { |tag, time, record|
       topic = record['topic'] || self.default_topic || tag
       partition = record['partition'] || self.default_partition
+#extra info feature,added by liyong
+      record["hostname"] = @host_local
+      record["localip"] = @ip_local
+      record["idc"] = @idc
+      record["platform"] = @platform  if @need_platform_info
+###################################
       message = Kafka::Message.new(record.to_s)
       records_by_topic[topic] ||= []
       records_by_topic[topic][partition] ||= []
@@ -45,15 +77,27 @@ class Fluent::KafkaOutput < Fluent::BufferedOutput
     records_by_topic.each { |topic, partitions|
       partitions.each_with_index { |messages, partition|
         next if not messages
-        config = {
-          :port      => self.port,
-          :host      => self.host,
-          :topic     => topic,
-          :partition => partition
-        }
-        #init producers
-        @producers[topic] ||= Kafka::Producer.new(config)
-        @producers[topic].push(messages)
+        if @zkhost and @zkport
+          config = {
+            :port      => @zkport,
+            :host      => @zkhost,
+            :topic     => topic,
+            :partition => partition
+          }
+          @producers[topic] ||= Kafka::ZKProducer.new(config)
+          producer.zkpush(messages)
+        elsif @host and @port
+          config = {
+            :port      => @port,
+            :host      => @host,
+            :topic     => topic,
+            :partition => partition
+          }
+          @producers[topic] ||= Kafka::Producer.new(config)
+          producer.push(messages)
+        else
+          puts "host or port argument is nil"
+        end
       }
     }
   end
